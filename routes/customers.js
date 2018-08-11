c70304258045231e2bf6787860339f282329cac8
nascal3 <nascal3@gmail.com>
2018-08-11 18:33:39 +0300

    added CRUD for managing customers

--- routes/customers.js
+++ routes/customers.js
@@ -6,6 +6,8 @@ const router = express.Router();
 const customerSchema = new mongoose.Schema({
     name: {
         type: String,
+        minLength: 3,
+        maxLength: 30,
         required: true
     },
     phone: {
@@ -20,7 +22,80 @@ const customerSchema = new mongoose.Schema({
 
 const Customer = mongoose.model('customers', customerSchema);
 
+// GET ALL CUSTOMERS
+router.get('/', async (req, res) => {
+    try {
+        const customers = await Customer.find().sort({name:1});
+        res.send(customers);
+    }catch (e) {
+        res.send(e.message);
+    }
+});
 
+// GET A SINGLE CUSTOMER
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
 
+    try {
+       const customer = await Customer.findById(id);
+       if (!customer) return res.status(404).send('Customer not found');
+
+       res.send(customer);
+    }catch (e) {
+        res.send(e.message);
+    }
+});
+
+// CREATE NEW CUSTOMER
+router.post('/', async (req, res) => {
+    try {
+        const customer = new Customer({
+            name: req.body.name,
+            phone: req.body.phone,
+            isGold: req.body.isGold
+        });
+
+        const results = await customer.save();
+        res.send(results);
+    }catch (e) {
+        res.send(e.message);
+    }
+});
+
+// UPDATE CUSTOMER DETAILS
+router.post('/:id', async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        let customerDetails = await Customer.findById(id);
+        if (!customerDetails) return res.status(404).send('Customer not found');
+
+        customerDetails.set({
+            name: req.body.name,
+            phone: req.body.phone,
+            isGold: req.body.isGold
+        });
+
+        const results = await customerDetails.save();
+        res.send(results);
+    }catch (e) {
+        res.send(e.message);
+    }
+});
+
+//  DELETE CUSTOMER
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+
+    try {
+      const customer = Customer.findById(id);
+      if (!customer) return res.status(404).send('Customer not found');
+
+      const results = await customer.remove();
+      res.send(results);
+    }catch (e) {
+        res.send(e.message);
+    }
+});
 
 module.exports = router;
