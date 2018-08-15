a9d89a08a7cf059f1797cedb919e5f3de1032af3
nascal3 <nascal3@gmail.com>
2018-08-16 00:57:10 +0300

    added movies model file

---
+++ models/movies.js
@@ -0,0 +1,34 @@
+const {genreSchema} = require('./genres');
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 30,
+        trim: true
+    },
+    numberInStock: {
+        type: Number,
+        default: 0,
+        min: 0,
+        max: 20
+    },
+    dailyRentalRate: {
+        type: Number,
+        default: 0,
+        min: 0,
+        max: 20
+    },
+    genre: {
+        type: genreSchema,
+        required: true,
+    }
+
+});
+
+const Movies = mongoose.model('movies', movieSchema);
+
+
+module.exports = Movies;
