c4ec3dbdcb977b722caca143a8b5b44cb0f46e60
nascal3 <nascal3@gmail.com>
2018-08-17 00:39:14 +0300

    added config valiables to auth file

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const config = require('config');
 const bcrypt = require('bcrypt');
 const {Users} = require('../models/users');
 const _ = require('lodash');
@@ -14,7 +15,7 @@ router.post('/', async (req, res) => {
         const validPassword = await bcrypt.compare(req.body.password, user.password);
         if (!validPassword) return res.status(400).send('Invalid email or password');
 
-        const token = jwt.sign({_id: user._id}, 'jwtPrivateKey');
+        const token = jwt.sign({_id: user._id}, config.get('jwtPrivateKey'));
         res.send(token);
     }catch (e) {
         res.send(e.message);
