ba141883802bb08c78a56ca7da28d934768eeddf
nascal3 <nascal3@gmail.com>
2018-08-16 23:18:27 +0300

    added user authentication proccess in auth file

---
+++ routes/auth.js
@@ -0,0 +1,22 @@
+const bcrypt = require('bcrypt');
+const {Users} = require('../models/users');
+const _ = require('lodash');
+const express = require('express');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+
+    try {
+        let user = await Users.findOne({email: req.body.email});
+        if (!user) return res.status(400).send('Invalid email or password');
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        if (!validPassword) return res.status(400).send('Invalid email or password');
+
+        res.send(true);
+    }catch (e) {
+        res.send(e.message);
+    }
+});
+
+module.exports = router;
