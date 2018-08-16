5553b86f5b7a2c17dfd061beb6c22d77d21e7e98
nascal3 <nascal3@gmail.com>
2018-08-16 14:28:30 +0300

    refactored code to use lodash library

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,18 @@
 const {Users} = require('../models/users');
+const _ = require('lodash');
 const express = require('express');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
 
     try {
-        const user = new Users({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
-        });
-        const results = await user.save();
-        res.send(results);
+        let user = await Users.findOne({email: req.body.email});
+        if (user) return res.status(400).send('the following user already exists');
+
+        user = new Users(_.pick(req.body, ['name', 'email', 'password']));
+        await user.save();
+
+        res.send(_.pick(user, ['_id','name', 'email']));
     }catch (e) {
         res.send(e.message);
     }
@@ -20,11 +21,11 @@ router.post('/', async (req, res) => {
 router.get('/', async (req, res) => {
 
     try {
-       const users = await Users.find().sort('name').populate({name:1, email: 1});
-        res.send(users);
+       const users = await Users.find().sort('name');
+       res.send(users);
     }catch (e) {
         res.send(e.message);
     }
 });
 
-module.exports.router = router;
+module.exports = router;
