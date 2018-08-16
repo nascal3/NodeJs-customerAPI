e25716b02bc2948963981f780a4259e5de79e5ee
nascal3 <nascal3@gmail.com>
2018-08-17 02:09:42 +0300

    removed unneccesary editing space

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,10 +3,10 @@ const config = require('config');
 
 function auth(req, res, next) {
     const token = req.header('x-auth-token');
-    if (!token) return res.status(401).send('Accsess denied, no token!');
+    if (!token) return res.status(401).send('Access denied, no token!');
 
     try {
-        req.user  = jwt.verify(token, config.get('jwtPrivateKey'));
+        req.user = jwt.verify(token, config.get('jwtPrivateKey'));
         next();
     }catch (e) {
         res.status(400).send('Invalid token');
