95766d926951be43fc8a865c43e909207fbb9561
nascal3 <nascal3@gmail.com>
2018-08-22 02:42:36 +0300

    removed code to group them in relevant files and add lines to refrence them

--- index.js
+++ index.js
@@ -1,40 +1,18 @@
-require('express-async-errors');
-const config = require('config');
 const express = require('express');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const genres = require('./routes/genres');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
-const error = require('./middleware/error');
 const logger = require('./middleware/logger');
-const mongoose = require('mongoose');
 const app = express();
 
-//GET UNCAUGHT ERRORS TO LOG FILE
-process.on('uncaughtException', (ex) => {
-    logger.error(ex.message, ex);
-});
+// CALL TO UNCAUGHT AND UNHANDLED
+require('./startup/logging')();
 
+// CALL TO DB CONNECTION FOLDER
+require('./startup/db')();
 
-if (!config.get('jwtPrivateKey')) {
-    console.error('FATAL ERROR: jwtPrivateKey Iis not defined!');
-    process.exit(1);
-}
+// CALL TO ROUTES FOLDER
+require('./startup/routes')(app);
 
-mongoose.connect('mongodb://localhost/vidly')
-    .then(() => console.log('Connection to DB established...'))
-    .catch(err => console.log(err.message));
+//CALL TO CONFIG FOLDER
+require('./startup/config')();
 
-app.use(express.json());
-app.use('/api/customers', customers);
-app.use('/api/genres', genres);
-app.use('/api/movies', movies);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-
-//error middleware
-app.use(error);
-
-const port = 3000;
-app.listen( port, () => console.log(`express running on port ${port}...`));
+const port = process.env.PORT || 3000;
+app.listen( port, () => logger.info(`express running on port ${port}...`));
