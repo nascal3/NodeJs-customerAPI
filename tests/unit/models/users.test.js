d5d82d0b9cc56fcc26f50927727e73eee0f4cec5
nascal3 <nascal3@gmail.com>
2018-08-23 16:37:37 +0300

    added config files to use for testing

---
+++ tests/unit/models/users.test.js
@@ -0,0 +1,9 @@
+
+const Users = require("../models/users");
+
+describe('generateAuthToken', () => {
+
+    it('generates a signed valid uer web token', ()=> {
+                Users.u
+    });
+});
