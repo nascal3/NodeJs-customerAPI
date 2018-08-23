cb5a63ae48effa0bd53b542db39da83073ec7ebb
nascal3 <nascal3@gmail.com>
2018-08-23 16:39:25 +0300

    added test for users JWT generation in models

--- tests/unit/models/users.test.js
+++ tests/unit/models/users.test.js
@@ -1,9 +1,19 @@
 
-const Users = require("../models/users");
+const {Users} = require("../../../models/users");
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const mongoose = require('mongoose');
 
 describe('generateAuthToken', () => {
 
-    it('generates a signed valid uer web token', ()=> {
-                Users.u
+    it('generates a signed valid user JSON web token', ()=> {
+        const payload = {
+            _id: new mongoose.Types.ObjectId().toHexString(),
+            isAdmin: true
+        };
+        const user = new Users(payload);
+        const token = user.generateAuthToken();
+        const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+        expect(decoded).toMatchObject(payload);
     });
 });
