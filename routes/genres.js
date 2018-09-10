b89cda33da506867733a725782658c92835442db
nascal3 <nascal3@gmail.com>
2018-09-10 23:01:45 +0300

    added line to make sure all id send are valid

--- routes/genres.js
+++ routes/genres.js
@@ -12,8 +12,9 @@ router.get('/', async (req, res, next) => {
 });
 
 router.get('/:id', async (req, res) => {
-
     const id = req.params.id;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('invalid ID');
 
     const result = await Genres.findById(id);
     if (!result) return res.status(404).send('genre not found');
