e2af368902d9d4133ca3dd4d8cb2a2714f84119d
nascal3 <nascal3@gmail.com>
2018-09-10 23:10:16 +0300

    added middleware to test valid object id

---
+++ middleware/validObjectId.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+module.exports = function (req, res, next) {
+    const id = req.params.id;
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('invalid ID');
+
+    next();
+}
