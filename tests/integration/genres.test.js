00eae0fa4e28d290daf8a6c6bfe4c345ec91a75a
nascal3 <nascal3@gmail.com>
2018-09-10 23:02:24 +0300

    added test file to test genres function

---
+++ tests/integration/genres.test.js
@@ -0,0 +1,51 @@
+const request = require('supertest');
+const { Genres } = require('../../models/genres');
+let server;
+
+describe('/api/genres', () =>{
+    beforeEach(() => { server = require('../../index'); });
+    afterEach( async() => {
+        server.close();
+        await Genres.remove({});
+    });
+
+    describe('GET/', () => {
+        it('should return all genres', async () => {
+
+            await Genres.collection.insertMany([
+                {name: 'genre1'},
+                {name: 'genre2'},
+            ]);
+
+           const res = await request(server).get('/api/genres');
+           expect(res.status).toBe(200);
+           expect(res.body.length).toBe(2);
+           expect(res.body.some(g => g.name === 'genre1')).toBeTruthy();
+
+        })
+    });
+
+    describe('GET/:id', ()=> {
+
+        it('should should return genre if valid id is passed', async () => {
+            const genre = new Genres({name: 'genre1'});
+            await genre.save();
+
+            const res = await request(server).get('/api/genres/'+ genre._id);
+
+            expect(res.status).toBe(200);
+            expect(res.body
+            ).toHaveProperty('name', genre.name);
+
+        });
+
+        it('should should return 404 if invalid id is passed', async () => {
+
+            const res = await request(server).get('/api/genres/1');
+            expect(res.status).toBe(404);
+
+        });
+
+    })
+
+});
