1f85f76ce0b0194650febdda4d054e0e4bd45724
nascal3 <nascal3@gmail.com>
2018-09-11 16:23:24 +0300

    added test for auth middleware

---
+++ tests/unit/middleware/auth.test.js
@@ -0,0 +1,22 @@
+const { Users } = require('../../../models/users');
+const auth = require('../../../middleware/auth');
+const mongoose = require('mongoose');
+
+describe('auth middleware', () => {
+
+    it('should populate req.user with the paylosd of a valid JWT', () => {
+
+        const user = { _id: mongoose.Types.ObjectId().toHexString(), isAdmin: true};
+        const token = new Users(user).generateAuthToken();
+        const req = {
+            header: jest.fn().mockReturnValue(token)
+        };
+        const res = {}
+        const next = jest.fn();
+        auth(req, res, next);
+
+        expect(req.user).toMatchObject(user);
+
+
+    });
+});
